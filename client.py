ff75afc63a834cff188052181e65aa823723ca13
Lima <felipebr4095@gmail.com>
2025-05-18 22:28:13 -0300

    update client.py

--- client.py
+++ client.py
@@ -2,12 +2,11 @@
 # -*- coding: utf-8 -*-
 #
 import socket
-import sys
 
 HOST = '127.0.0.1'
 PORT = 10142
 
-mensagem = ' '.join(sys.argv[1:])
+mensagem = input("");
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
