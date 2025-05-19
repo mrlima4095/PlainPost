a9e7c42d7bdc48c0519fae35d79657763f77fdbf
Lima <felipebr4095@gmail.com>
2025-05-19 13:48:00 -0300

    keep alive

--- client.py
+++ client.py
@@ -6,10 +6,13 @@ import socket
 HOST = '127.0.0.1'
 PORT = 10142
 
-mensagem = input("");
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(mensagem.encode())
+    while True:
+        try: 
+            mensagem = input("");
+            s.sendall(mensagem.encode())
 
-    print(s.recv(4096).decode())
+            print(s.recv(4096).decode())
