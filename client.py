a056cc86ed49b9e7d5653a1ac115ec3b1ade89a0
Mr Lima <felipebr4095@gmail.com>
2025-05-19 22:21:51 -0300

    global vps ip client

--- client.py
+++ client.py
@@ -3,16 +3,14 @@
 #
 import socket
 
-HOST = '127.0.0.1'
+HOST = '31.97.20.160'
 PORT = 10142
-
-
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     while True:
         try: 
-            mensagem = input("");
+            mensagem = input("> ");
             s.sendall(mensagem.encode())
 
             print(s.recv(4096).decode())
