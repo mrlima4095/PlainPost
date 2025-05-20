10a1b7903b5a0bbcfbd40485d702ae89399df7db
Lima <felipebr4095@gmail.com>
2025-05-19 22:41:04 -0300

    update client.py

--- client.py
+++ client.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
+
 import socket
+import getpass
 
 HOST = '31.97.20.160'
 PORT = 10142
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    while True:
-        try: 
-            mensagem = input("> ");
-            s.sendall(mensagem.encode())
+USERNAME = input("User: ")
+PASSWORD = input("Password: ")
 
-            print(s.recv(4096).decode())
+    
+    
+class Client:
+    def __init__(self):
+        self.username = input("Username: ").strip()
+        self.password = getpass.getpass("Password: ").strip()
+        
+        self.payload = {"username": self.username, "password": self.password}
+    def request(self, payload)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((HOST, PORT))
+            
+        s.sendall(payload.encode())
+        return s.recv(4096).decode()
