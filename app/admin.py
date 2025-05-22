15ea0511759dcc97231ef5a4779373e9daf6990c
Mr Lima <felipebr4095@gmail.com>
2025-05-22 18:40:22 -0300

    x

--- app/admin.py
+++ app/admin.py
@@ -65,6 +65,7 @@ class AdminPanel:
         for row in self.cursor.fetchall():
             print(f"[+] {row['username']} (Coins: {row['coins']}) - {row['biography']}")
 
+
     # Mails
     def send(self, target, content):
         self.cursor.execute("SELECT * FROM users WHERE username = ?", (target,))
