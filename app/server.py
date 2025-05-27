96b87ab1c4a14aa9588eab8f82efdd4557b5c107
Mr Lima <felipebr4095@gmail.com>
2025-05-27 09:17:57 -0300

    47

--- app/server.py
+++ app/server.py
@@ -178,6 +178,9 @@ def mail():
 
         return jsonify({"response": "Password changed!"}), 200
     elif payload['action'] == "search":
+        mailcursor.execute("SELECT role FROM users WHERE username = ?", (payload['user'],))
+        row = mailcursor.fetchone()
+
         if row: return jsonify({"response": f"[{row['role']}] {payload['user']}"}), 200 
         else: return jsonify({"response": "Target not found!"}), 404
     elif payload['action'] == "me":
@@ -213,7 +216,7 @@ def mail():
 
         mailcursor.execute("SELECT coins FROM users WHERE username = ?", (username,))
         user_row = mailcursor.fetchone()
-        if user_row["coins"] < price: return jsonify({"response": "No enough money!"}), 401
+        if user_row["coins"] < price: return jsonify({"response": "No enough money!"}), 401 
         mailcursor.execute("INSERT INTO user_roles (username, role) VALUES (?, ?)", (username, request['role']))
         mailcursor.execute("UPDATE users SET coins = coins - ? WHERE username = ?", (price, username))
         mailserver.commit()
