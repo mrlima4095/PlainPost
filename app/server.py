6bf673c85877427a8bdf4a31f949d7a15be48801
Lima <felipebr4095@gmail.com>
2025-05-28 07:42:35 -0300

    update server.py

--- app/server.py
+++ app/server.py
@@ -269,8 +269,7 @@ def drive_download(file_id):
     original_name, saved_name = row
     path = os.path.join(UPLOAD_FOLDER, saved_name)
     return send_file(path, as_attachment=True, download_name=original_name)
-# |
-
+# | (GET file lists)
 @app.route('/api/drive/list', methods=['GET'])
 def drive_list():
     username = get_user(request.headers.get("Authorization"))
@@ -290,7 +289,7 @@ def drive_list():
             "expire_time": row[4]
         })
     return jsonify(result), 200
-
+# | (DELETE files)
 @app.route('/api/drive/delete/<file_id>', methods=['DELETE'])
 def drive_delete(file_id):
     mailserver, mailcursor = getdb()
