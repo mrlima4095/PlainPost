5d304290c350cde936e9da19abf77b0826ba24a0
Mr Lima <felipebr4095@gmail.com>
2025-05-28 17:18:28 -0300

    120

--- scripts/options.js
+++ scripts/options.js
@@ -25,7 +25,7 @@ window.onload = () => {
         const { value: content } = await Swal.fire({ title: 'Biografia:', input: 'text', inputPlaceholder: 'O que esta pensando?', showCancelButton: true });
         if (!content) return Swal.fire('Erro', 'Sua biografia não pode estar vazia!', 'error');
 
-        const { status } = await fetchRequest("changebio", { bio: content });
+        const { status } = await conectar("changebio", { bio: content });
         if (status === 200) Swal.fire('Sucesso', 'Sua biografia foi alterada!', 'success');
         else if (status === 404) Swal.fire('Erro', 'O destinatário não foi encontrado!', 'error');
         else Swal.fire('Erro', 'Erro ao alterar sua biografia.', 'error');
