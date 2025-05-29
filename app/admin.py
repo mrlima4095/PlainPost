af6072183a9438136eef4770dee5a6322ab97359
Lima <felipebr4095@gmail.com>
2025-05-28 23:20:10 -0300

    update admin.py

--- app/admin.py
+++ app/admin.py
@@ -124,6 +124,7 @@ class AdminPanel:
         self.db.commit()
         print("[~] All emails cleared from database.")
     # |
+    # Coins API - GIVE/ TAKE
     def give_coins(self, username, amount):
         self.cursor.execute("UPDATE users SET coins = coins + ? WHERE username = ?", (amount, username))
         self.db.commit()
