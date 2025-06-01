2e172ba019f0ac1e7a2e8f5aa793bb34c5daa49c
Lima <felipebr4095@gmail.com>
2025-06-01 17:44:28 -0300

    update options.js

--- scripts/options.js
+++ scripts/options.js
@@ -3,7 +3,7 @@ window.onload = () => {
     if (!token) { window.location.href = "login"; return; }
 
     document.getElementById("back").addEventListener("click", () => { window.location.href = "/mail"; });   
-    document.getElementById("mural").addEventListener("click", async () => { 
+    /*document.getElementById("mural").addEventListener("click", async () => { 
         try {
             const resposta = await fetch("https://servidordomal.fun/api/mail", { method: "POST", headers: { "Content-Type": "application/json", "Authorization": token }, body: JSON.stringify({ action: "status" }), });
             const dados = await resposta.json();
@@ -12,7 +12,7 @@ window.onload = () => {
             else if (resposta.status == 401) window.location.href = "/mail";
             else { Swal.fire('Erro', 'Ocorreu um erro interno.', 'error'); }
         } catch { Swal.fire('Erro', 'Erro na conexão.', 'error'); }
-    });   
+    });*/   
     document.getElementById("changebio").addEventListener("click", async () => {
         const { value: content } = await Swal.fire({ title: 'Biografia:', input: 'text', inputPlaceholder: 'O que esta pensando?', showCancelButton: true });
         if (!content) return Swal.fire('Erro', 'Sua biografia não pode estar vazia!', 'error');
@@ -24,7 +24,7 @@ window.onload = () => {
             else if (resposta.status == 401) Swal.fire('Erro', 'O destinatário não foi encontrado!', 'error');
             else { Swal.fire('Erro', 'Erro ao alterar sua biografia.', 'error'); }
         } catch { Swal.fire('Erro', 'Erro na conexão.', 'error'); }
-    });
+    });/*
     document.getElementById("changepage").addEventListener("click", async () => {
         const { value: file_id } = await Swal.fire({ title: 'ID do arquivo:', input: 'text', inputPlaceholder: 'Link do arquivo do BinDrop', showCancelButton: true });
         if (!file_id) return Swal.fire('Erro', 'O ID não pode estar vazio!', 'error');
@@ -37,7 +37,7 @@ window.onload = () => {
             else if (resposta.status == 410) Swal.fire('Erro', 'O arquivo não esta disponivel!', 'error');
             else { Swal.fire('Erro', 'Erro ao alterar a pagina.', 'error'); }
         } catch { Swal.fire('Erro', 'Erro na conexão.', 'error'); }
-    });
+    });*/
 
 }
 //function compartilhar(fileId) { const link = `${apiBase}/download/${fileId}`; navigator.clipboard.writeText(link).then(() => { Swal.fire("Link copiado!", "O link de download foi copiado para a área de transferência.", "success"); }).catch(() => { Swal.fire("Erro", "Não foi possível copiar o link.", "error"); }); }
