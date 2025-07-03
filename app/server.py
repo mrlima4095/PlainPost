5039a6b26d1e0b318baa1ee0faa79f1c8f1931a0
Lima <felipebr4095@gmail.com>
2025-07-02 23:14:46 -0300

    update server.py

--- app/server.py
+++ app/server.py
@@ -536,7 +536,7 @@ def short_links_handler():
         mailcursor.execute("SELECT role, coins FROM users WHERE username = ?", (username,))
         row = mailcursor.fetchone()
         role = row['role']
-        coins = row['coins'] 44
+        coins = row['coins'] 45
 
         if role == "user":
             mailcursor.execute("SELECT COUNT(*) FROM short_links WHERE owner = ?", (username,))
