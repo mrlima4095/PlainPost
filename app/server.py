000d484a84e1066a0c988d242bd137c9abf51178
Mr Lima <felipebr4095@gmail.com>
2025-07-02 21:14:03 -0300

    NO ROUTE USE MAIl

--- app/server.py
+++ app/server.py
@@ -145,13 +145,6 @@ def signup():
         max_age=60*60*24*7
     )
     return response
-# | (Logout)
-@app.route('/api/logout', methods=['POST'])
-def logout():
-    response = make_response(jsonify({"response": "Logged out successfully."}), 200)
-    response.set_cookie('token', '', max_age=0, httponly=True, secure=True, samesite='Lax', path='/')
-
-    return response
 # |
 # Social API
 # | (Main Handler)
