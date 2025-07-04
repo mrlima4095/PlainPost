ae1b2d22d4b4ae234c47a621a08c33e07e4fbba0
Mr Lima <felipebr4095@gmail.com>
2025-07-04 17:54:03 -0300

    search prevent others

--- app/server.py
+++ app/server.py
@@ -260,7 +260,12 @@ def mail():
 
         return jsonify({"response": "OK"}), 200
     elif payload['action'] == "search":
-        mailcursor.execute("SELECT role, biography FROM users WHERE username = ?", (payload['user'],))
+        user = payload.get('user')
+
+        if to.endswith("@archsource.xyz"): to = to.replace("@archsource.xyz", "")
+        elif "@" in to: return jsonify({"response": "Only supported to other PlainPost users!"}), 405
+
+        mailcursor.execute("SELECT role, biography FROM users WHERE username = ?", (,))
         row = mailcursor.fetchone()
 
         if row: return jsonify({"response": f"ID: {payload['user']}\nRole: [{row['role']}]\nBio: {row['biography']}"}), 200 
